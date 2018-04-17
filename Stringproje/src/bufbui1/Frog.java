aa4954b1df5ae84c69fb012a4dd9c5e7d28d199a
preethamb <preethamb656@gmail.com>
2018-04-17 06:32:28 -0700

    Add files via upload

---
+++ Stringproje/src/bufbui1/Frog.java
@@ -0,0 +1,20 @@
+package bufbui1;
+
+public class Frog {
+	
+	
+	/*public String toString() {
+		
+		return "Hello";
+	}
+	*/
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Frog frog1=new Frog();
+		
+		
+System.out.println(frog1);
+	}
+
+}
